d6052eab9df2ce21fb4b7f003105fd7285e57ac5
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-23 12:53:52 +0300

    Create N2.cpp

---
+++ N2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russia");
+	string nedelya[7] = { "sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday" };
+	int n;
+	cin >> n;
+	n = n % 6;
+	if (n != 3) cout << nedelya[n] << endl;
+	else cout << "It is " << nedelya[n] << " my dude" << endl;
+	system("pause");
+	return 0;
+}
