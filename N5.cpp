0bb410d4564d750289b9967b3171cbfb71a3ea02
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-23 13:14:10 +0300

    Create N5.cpp

---
+++ N5.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int year, cent;
+	cin >> year;
+	cent = year / 100;
+	if (year % 100
+		== 0) cout << (cent - 1) << endl;
+	else cout << cent << endl;
+	system("pause");
+	return 0;
+}
